d258483eff8e99de346f025ca5894cb66a2a2fd4
Marc Monteu <marc.monteu@gmail.com>
2019-11-02 15:50:30 +0100

    modif des articles et ajout de la bdd en .sql

---
+++ sites/default/files/php/twig/5dbd89e4e9bb2_menu.html.twig_aXdJ612hYVBfd4JxkSVyW40Nz/07EMC0kX9693kGKAp6FogqooTWYEusHIIgcZ3lPUKwQ.php
@@ -0,0 +1,202 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/bootstrap/templates/menu/menu.html.twig */
+class __TwigTemplate_de77edcb8c6f4316c19b520dbff2313c3ed1e4196f278acd26429fdd4aee004a extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["import" => 18, "macro" => 27, "if" => 29, "for" => 35, "set" => 37];
+        $filters = ["clean_class" => 25, "escape" => 31];
+        $functions = ["link" => 48];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['import', 'macro', 'if', 'for', 'set'],
+                ['clean_class', 'escape'],
+                ['link']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 18
+        $context["menus"] = $this;
+        // line 19
+        echo "
+";
+        // line 24
+        echo "
+";
+        // line 25
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($context["menus"]->getmenu_links(($context["items"] ?? null), ($context["attributes"] ?? null), 0, \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["menu_name"] ?? null)))));
+        echo "
+
+";
+    }
+
+    // line 27
+    public function getmenu_links($__items__ = null, $__attributes__ = null, $__menu_level__ = null, $__menu_name__ = null, ...$__varargs__)
+    {
+        $context = $this->env->mergeGlobals([
+            "items" => $__items__,
+            "attributes" => $__attributes__,
+            "menu_level" => $__menu_level__,
+            "menu_name" => $__menu_name__,
+            "varargs" => $__varargs__,
+        ]);
+
+        $blocks = [];
+
+        ob_start();
+        try {
+            // line 28
+            echo "  ";
+            $context["menus"] = $this;
+            // line 29
+            echo "  ";
+            if (($context["items"] ?? null)) {
+                // line 30
+                echo "    ";
+                if ((($context["menu_level"] ?? null) == 0)) {
+                    // line 31
+                    echo "      <ul";
+                    echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => "menu", 1 => "nav", 2 => ($context["menu_name"] ?? null)], "method")), "html", null, true);
+                    echo ">
+    ";
+                } else {
+                    // line 33
+                    echo "      <ul";
+                    echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => "dropdown-menu", 1 => ($context["menu_name"] ?? null)], "method")), "html", null, true);
+                    echo ">
+    ";
+                }
+                // line 35
+                echo "    ";
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+                foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                    // line 36
+                    echo "      ";
+                    // line 37
+                    $context["item_classes"] = [0 => (($this->getAttribute(                    // line 38
+$context["item"], "is_expanded", [])) ? ("expanded") : ("")), 1 => ((($this->getAttribute(                    // line 39
+$context["item"], "is_expanded", []) && (($context["menu_level"] ?? null) == 0))) ? ("dropdown") : ("")), 2 => (($this->getAttribute(                    // line 40
+$context["item"], "in_active_trail", [])) ? ("active") : (""))];
+                    // line 43
+                    echo "      ";
+                    if (((($context["menu_level"] ?? null) == 0) && $this->getAttribute($context["item"], "is_expanded", []))) {
+                        // line 44
+                        echo "        <li";
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($this->getAttribute($context["item"], "attributes", []), "addClass", [0 => ($context["item_classes"] ?? null)], "method")), "html", null, true);
+                        echo ">
+        <a href=\"";
+                        // line 45
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "url", [])), "html", null, true);
+                        echo "\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">";
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "title", [])), "html", null, true);
+                        echo " <span class=\"caret\"></span></a>
+      ";
+                    } else {
+                        // line 47
+                        echo "        <li";
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($this->getAttribute($context["item"], "attributes", []), "addClass", [0 => ($context["item_classes"] ?? null)], "method")), "html", null, true);
+                        echo ">
+        ";
+                        // line 48
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->env->getExtension('Drupal\Core\Template\TwigExtension')->getLink($this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "title", [])), $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "url", []))), "html", null, true);
+                        echo "
+      ";
+                    }
+                    // line 50
+                    echo "      ";
+                    if ($this->getAttribute($context["item"], "below", [])) {
+                        // line 51
+                        echo "        ";
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($context["menus"]->getmenu_links($this->getAttribute($context["item"], "below", []), $this->getAttribute(($context["attributes"] ?? null), "removeClass", [0 => "nav"], "method"), (($context["menu_level"] ?? null) + 1), ($context["menu_name"] ?? null)));
+                        echo "
+      ";
+                    }
+                    // line 53
+                    echo "      </li>
+    ";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 55
+                echo "    </ul>
+  ";
+            }
+        } catch (\Exception $e) {
+            ob_end_clean();
+
+            throw $e;
+        } catch (\Throwable $e) {
+            ob_end_clean();
+
+            throw $e;
+        }
+
+        return ('' === $tmp = ob_get_clean()) ? '' : new Markup($tmp, $this->env->getCharset());
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/bootstrap/templates/menu/menu.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  159 => 55,  152 => 53,  146 => 51,  143 => 50,  138 => 48,  133 => 47,  126 => 45,  121 => 44,  118 => 43,  116 => 40,  115 => 39,  114 => 38,  113 => 37,  111 => 36,  106 => 35,  100 => 33,  94 => 31,  91 => 30,  88 => 29,  85 => 28,  70 => 27,  63 => 25,  60 => 24,  57 => 19,  55 => 18,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/bootstrap/templates/menu/menu.html.twig", "C:\\WampServer\\wamp64\\www\\drupal-demo\\themes\\bootstrap\\templates\\menu\\menu.html.twig");
+    }
+}
